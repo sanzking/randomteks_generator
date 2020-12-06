803856b65d6c2425501008de6a876188e256acd7
Muhammad Shohibul Faqih <70243455+sanzking@users.noreply.github.com>
2020-12-06 09:39:39 +0700

    Add files via upload

---
+++ teksrandom.py
@@ -0,0 +1,9 @@
+import random
+import os
+#random teks script : sanzking
+#main
+#ubah contoh1-10 menjadi teks yang kalian inginkan.
+teks = ['contoh1','contoh2','contoh3','contoh4','contoh5','contoh6','cobtoh7','contoh8','contoh9','contoh10']
+konek = random.choice(teks)
+#eksekusi
+print(konek)
